021e4d23e33bb6c725fee5868a3b2fce88f69bb2
fgorgojomarugan <f.gorgojo.marugan@accenture.com>
2026-03-13 11:39:29 +0000

    exercise 5

---
+++ octofit-tracker/frontend/octofit-tracker/frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="OctoFit Logo" />
+        <h1>OctoFit Tracker</h1>
+      </header>
+      <main>
+        <p className="App-link">Welcome to the OctoFit Tracker app!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
